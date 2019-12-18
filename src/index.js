e57e4b056f253b0deb5a9273416a79c2726f10cf
PianoFF <Fangfei1star@gmail.com>
2019-12-18 17:20:55 +0000

    more buttons

--- src/index.js
+++ src/index.js
@@ -6,8 +6,37 @@ document.addEventListener("DOMContentLoaded", function(){
     const ul = document.createElement('ul');
     ul.id = "all-films"; 
     body.insertBefore(ul, script); 
+    
+    //header
+    const header = document.createElement('header');
+    body.insertBefore(header, ul); 
+    
+    header.classList = 'running-header';
 
-    // const br = document.createElement('br'); 
+    //header content
+    const searchBox = document.createElement('input');
+    header.appendChild(searchBox); 
+    searchBox.placeholder = 'search';
+
+    const searchSelection = document.createElement('select');
+    header.appendChild(searchSelection); 
+    searchSelection.add(new Option("Title",1)); 
+    searchSelection.add(new Option("Director", 2)); 
+    searchSelection.add(new Option("Producer",3)); 
+    
+    const searchButton = document.createElement('button');
+    header.appendChild(searchButton);
+    searchButton.classList = 'search-button header-btn'; 
+    searchButton.innerText = 'Submit'; 
+
+    // create a film _ with user input
+    const newFilmButton = document.createElement('button');
+    header.appendChild(newFilmButton);
+    newFilmButton.innerText = "Create A New Film"; 
+    newFilmButton.classList = 'header-btn';
+
+    
+    //end of header
 
     //'create' _ display all content
     const createAFilmEntry = function(film){
@@ -22,6 +51,7 @@ document.addEventListener("DOMContentLoaded", function(){
         const p = document.createElement('p');
         p.innerText = `Director: ${film.director} \n Producer: ${film.producer} \n Released on: ${film.release_date}\n Description: ${film.description}`;
         li.appendChild(p);
+
         //update button;
         const updateBtn = document.createElement("button");
         li.appendChild(updateBtn); 
